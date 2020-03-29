47f709ffa5b6b4d050637a8b4ae8547f0c70aa76
Sam Melton <sam@oddlots.org>
2020-03-29 17:10:23 -0500

    remove defunct cask cleanup and added doc

--- bin/clean-update.sh
+++ bin/clean-update.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
+echo "--------------------------"
 echo "system updates"
+echo "--------------------------"
 sudo softwareupdate -i -a
 
+echo "--------------------------"
 echo "brew cleanup and updates"
+echo "--------------------------"
 brew update
 brew upgrade
 brew cleanup -s
-brew cask cleanup
 
+echo "--------------------------"
 echo "brew diagnotic stuff"
+echo "--------------------------"
 brew doctor
 brew missing
 
+echo "--------------------------"
 echo "Atom upgrade stuff"
+echo "--------------------------"
 apm upgrade -c false
 
 #echo "NPM update stuff"
 #npm update -g
+echo "--------------------------"
 echo "Yarn update stuff"
+echo "--------------------------"
 yarn global upgrade;
 
+echo "--------------------------"
 echo "Ruby gems update stuff"
+echo "--------------------------"
 sudo gem update --system
 sudo gem update
 sudo gem cleanup
